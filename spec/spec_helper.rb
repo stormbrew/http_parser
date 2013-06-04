7d15160b52077d4aea5e01f61863472d765dbc34
Praveen Arimbrathodiyil <praveen@debian.org>
2013-06-04 13:27:08 +0530

    update spec for rspec 2
    require 'spec' is require 'rspec' now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'spec'
-require 'spec/autorun'
 
-Spec::Runner.configure do |config|
+require 'rspec'
+
+Rspec.configure do |config|
   
 end
